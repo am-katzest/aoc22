b2accde84a547d7455dbd4f78dbedeabb703bfdc
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-02 20:27:27 +0100

    ..

--- src/bored/core.clj
+++ src/bored/core.clj
@@ -1,14 +1,21 @@
 (ns bored.core
   (:require [clojure.string :as s]))
+(def val {:rock 1
+          :paper 2
+          :scissors 3})
+(def  trans {"X" :rock "A" :rock
+             "Y" :paper "B" :paper
+             "Z" :scissors "C" :scissors})
+(def wins {:rock :scissors
+           :paper :rock
+           :scissors :paper})
+(defn line->score [l]
+  (let [[opp my] (map trans (s/split l #" "))]
+    (+ (my val) (cond (= opp my) 3
+                      (= (wins my) opp) 6
+                      :else 0))))
 (->> "input"
      slurp
      s/split-lines
-     (partition-by #{""})
-     (remove #{'("")})
-     (map (fn [x]
-            (->> x
-                 (map #(Integer/parseInt %))
-                 (reduce +))))
-     (sort >)
-     (take 3)
+     (map line->score)
      (reduce +))

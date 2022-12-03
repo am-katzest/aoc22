4554aa0e93863284db1a983c087876dbb58c4e86
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-03 18:52:51 +0100

    ..

--- src/bored/core.clj
+++ src/bored/core.clj
@@ -1,21 +1,42 @@
 (ns bored.core
   (:require [clojure.string :as s]))
-(def val {:rock 1
-          :paper 2
-          :scissors 3})
-(def  trans {"X" :rock "A" :rock
-             "Y" :paper "B" :paper
-             "Z" :scissors "C" :scissors})
-(def wins {:rock :scissors
-           :paper :rock
-           :scissors :paper})
+temptation
+
+(def val {:r 1
+          :p 2
+          :s 3})
+(def trans {"X" :r "A" :r
+            "Y" :p "B" :p
+            "Z" :s "C" :s})
+(def wins {:r :s
+           :p :r
+           :s :p})
 (defn line->score [l]
   (let [[opp my] (map trans (s/split l #" "))]
     (+ (my val) (cond (= opp my) 3
                       (= (wins my) opp) 6
                       :else 0))))
-(->> "input"
+(->> "input2"
      slurp
      s/split-lines
      (map line->score)
+     (reduce +))
+
+(def loses {:s :r
+            :r :p
+            :p :s})
+
+(def trans2 {"X" wins "A" :r
+             "Y" identity "B" :p
+             "Z" loses "C" :s})
+(defn line->score2 [l]
+  (let [[opp strategy] (map trans2 (s/split l #" "))
+        my (strategy opp)]
+    (+ (my val) (cond (= opp my) 3
+                      (= (wins my) opp) 6
+                      :else 0))))
+(->> "input2"
+     slurp
+     s/split-lines
+     (map line->score2)
      (reduce +))

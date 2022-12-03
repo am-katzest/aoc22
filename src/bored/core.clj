bdd4a9256cd99e78ecb602ae3f763ea1eba4164c
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-03 19:39:56 +0100

    ..

--- src/bored/core.clj
+++ src/bored/core.clj
@@ -3,22 +3,29 @@
             [clojure.set :as set]))
 
 (defn split-sack [s]
-  (map set (split-at (/ (count s) 2) s)))
+  (split-at (/ (count s) 2) s))
 (defn item->p [x]
   (let [i (int x)
         lowercase (> i 95)]
     (if lowercase
       (+ 1 (- i (int \a)))
       (+ 27 (- i (int \A))))))
-(set/intersection #{1 2} #{3 5})
-(item->p \z)
-(defn line->p [line]
-  (let []))
 (->> "input3b"
      slurp
      s/split-lines
      (map #(->> %
                 split-sack
+                (map set)
+                (apply set/intersection)
+                first
+                item->p))
+     (reduce +))
+(->> "input3b"
+     slurp
+     s/split-lines
+     (map set)
+     (partition 3)
+     (map #(->> %
                 (apply set/intersection)
                 first
                 item->p))

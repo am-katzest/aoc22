584638140206675e1fba1d50ba1676c9efc310dc
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-05 15:48:31 +0100

    ...

--- src/bored/core.clj
+++ src/bored/core.clj
@@ -2,31 +2,22 @@
   (:require [clojure.string :as s]
             [clojure.set :as set]))
 
-(defn split-sack [s]
-  (split-at (/ (count s) 2) s))
-(defn item->p [x]
-  (let [i (int x)
-        lowercase (> i 95)]
-    (if lowercase
-      (+ 1 (- i (int \a)))
-      (+ 27 (- i (int \A))))))
-(->> "input3b"
+(defn  contain? [[[a A] [b B]]]
+  (or (and (>= a b) (>= B A))
+      (and (>= b a) (>= A B))))
+
+(defn overlap? [[[a A] [b B]]]
+  (or (<= a b A)
+      (<= a B A)
+      (<= b a B)))
+
+(defn parse-line [line]
+  (->> (s/split line #"[,-]")
+       (map #(Integer/parseInt %))
+       (partition 2)))
+
+(->> "input4b"
      slurp
      s/split-lines
-     (map #(->> %
-                split-sack
-                (map set)
-                (apply set/intersection)
-                first
-                item->p))
-     (reduce +))
-(->> "input3b"
-     slurp
-     s/split-lines
-     (partition 3)
-     (map #(->> %
-                (map set)
-                (apply set/intersection)
-                first
-                item->p))
-     (reduce +))
+     (filter (comp  overlap? parse-line))
+     count)

48723c515790ea61a66096771c59ad508ee88ce5
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-07 17:25:48 +0100

    ...

--- src/bored/core.clj
+++ src/bored/core.clj
@@ -6,29 +6,28 @@
 (defn back? [x] (= "$ cd .." x))
 (defn file? [l] (when-let [[_ size _] (re-matches #"([0-9]+) (.+)" l)]
                   (Integer/parseInt size)))
-(defn deltas [path size]
-  (loop [acc {}
-         path path]
-    (if (empty? path) acc
-        (recur (merge acc {path size}) (pop path)))))
 
 (defn sum-sizes [input]
   (loop [[l & r] input
-         acc {}
-         path []]
+         acc []
+         sum 0]
     (cond
-      (nil? l) acc
-      (back? l) (recur r acc (pop path))
-      (cd? l) (recur r acc (conj path (cd? l)))
-      (file? l) (recur r (merge-with + acc (deltas path (file? l))) path)
-      :else (recur r acc path))))
+      (or (nil? l) (back? l)) [r acc sum]
+      (cd? l) (let [[r a s] (sum-sizes r)
+                    sum' (+ sum s)
+                    acc' (concat acc a [s])]
+                (if r
+                  (recur r acc' sum')
+                  [r acc' sum']))
+      (file? l) (recur r acc (+ sum (file? l)))
+      :else (recur r acc sum))))
 
 (let [sizes
       (->> "input7b"
            slurp
            s/split-lines
            sum-sizes
-           (map second))
+           second)
       free-so-far (- 70000000 (apply max sizes))
       need-free (-  30000000  free-so-far)]
   {:first      (->> sizes

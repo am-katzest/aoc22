37650f3833021ec79fa8ed7a6535d44c1a5f824b
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-07 14:03:21 +0100

    ..

--- src/bored/core.clj
+++ src/bored/core.clj
@@ -2,18 +2,43 @@
   (:require
    [clojure.string :as s]))
 
-(defn index-of [p coll]
-  (loop [i 0 [h & t] coll]
-    (if (p h) i
-        (recur (inc i) t))))
+(defn cd? [l] (second (re-find #"\$ cd (.*)" l)))
+(defn back? [x] (= "$ cd .." x))
+(defn ls? [l] (re-matches #"\$ ls" l))
+(defn dir? [l] (when-let [[_ d] (re-find #"dir (.+)" l)] {d {}}))
+(defn file? [l] (when-let [[_ size name] (re-matches #"([0-9]+) (.+)" l)]
+                  {name (Integer/parseInt size)}))
 
-(defn all-unique? [coll]
-  (= (count coll)
-     (count (distinct coll))))
+(defn make-hierarchy [input]
+  (loop [[l & r] input
+         acc {}
+         path []]
+    (cond
+      (nil? l) acc
+      (back? l) (recur r acc (pop path))
+      (cd? l) (recur r acc (conj path (cd? l)))
+      (ls? l) (recur r acc path)
+      :else (recur r (update-in acc path merge
+                                (or (file? l) (dir? l))) path))))
 
-(defn packet-start [len msg]
-  (->> msg (partition len 1) (index-of all-unique?) (+ len)))
+(defn sum-dir-sizes [map]
+  (loop [[[k v] & r] (seq map)
+         dirs []
+         size 0]
+    (cond
+      (nil? v) [size dirs]
+      (int? v) (recur r dirs (+ size v))
+      :else (let [[s d] (sum-dir-sizes v)]
+              (recur r (concat dirs d {k s}) (+ s size))))))
 
-(->> "input6b" slurp (packet-start 4))
-
-(->> "input6b" slurp (packet-start 14))
+(let [[root rest] (->> "input7b"
+                       slurp
+                       s/split-lines
+                       make-hierarchy
+                       sum-dir-sizes)
+      free-so-far (- 70000000 root)
+      need-free (-  30000000  free-so-far)]
+  (->> rest
+       (map second)
+       (filter #(<= need-free %))
+       (apply min)))

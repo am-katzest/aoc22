08cc5cfcd1077aa66475e39001a434ec9e986e05
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-08 13:37:30 +0100

    ugh

--- src/bored/core.clj
+++ src/bored/core.clj
@@ -2,37 +2,32 @@
   (:require
    [clojure.string :as s]))
 
-(defn cd? [l] (second (re-find #"\$ cd (.*)" l)))
-(defn back? [x] (= "$ cd .." x))
-(defn file? [l] (when-let [[_ size _] (re-matches #"([0-9]+) (.+)" l)]
-                  (Integer/parseInt size)))
+(defn biggest-so-far [coll]
+  (let [[f _ & rest] (reductions (fn [[_ max] item]
+                                   (if (> item max)
+                                     [true item]
+                                     [false max]))
+                                 [true Integer/MIN_VALUE] coll)]
+    (map first (cons f rest))))
 
-(defn sum-sizes [input]
-  (loop [[l & r] input
-         acc []
-         sum 0]
-    (cond
-      (or (nil? l) (back? l)) [r acc sum]
-      (cd? l) (let [[r a s] (sum-sizes r)
-                    sum' (+ sum s)
-                    acc' (concat acc a [s])]
-                (if r
-                  (recur r acc' sum')
-                  [r acc' sum']))
-      (file? l) (recur r acc (+ sum (file? l)))
-      :else (recur r acc sum))))
+(defn transpose [x] (apply mapv vector x))
 
-(let [sizes
-      (->> "input7b"
-           slurp
-           s/split-lines
-           sum-sizes
-           second)
-      free-so-far (- 70000000 (apply max sizes))
-      need-free (-  30000000  free-so-far)]
-  {:first      (->> sizes
-                    (filter #(>= 100000 %))
-                    (reduce +))
-   :second (->> sizes
-                (filter #(<= need-free %))
-                (apply min))})
+(defn check-visibility [arr]
+  (let [tran #(comp transpose % transpose)
+        iter #(mapv biggest-so-far %)
+        rev #(mapv (comp reverse biggest-so-far reverse) %)
+        a (iter arr)
+        b (rev arr)
+        c ((tran iter) arr)
+        d ((tran rev) arr)
+        aa #(or %1 %2 %3 %4)]
+    (mapv #(mapv aa %1 %2 %3 %4) a b c d)))
+
+(->> "input8b"
+     slurp
+     (s/split-lines)
+     (mapv (fn [l] (mapv #(Integer/parseInt (str %)) l)))
+     check-visibility
+     (reduce concat)
+     (filter identity)
+     count)

11c5cfbff3439b19957adcc97d97f86448fb482d
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-10 15:58:13 +0100

    ...

--- src/bored/10.clj
+++ src/bored/10.clj
@@ -10,25 +10,23 @@
   (if op (let [ctr' (+ op ctr)] [ctr' [ctr ctr']])
       [ctr [ctr]]))
 
-(defn interesting? [i] (= 20 (mod i 40)))
+(defn draw-pixel [i v] (if (#{-1 0 1} (- i v)) "#" "."))
 
-(defn covered? [i v] (#{1 0 -1} (- v (mod i 40))))
+(defn interesting-value [i r] (* (+ 20 (* 40 i)) (nth r 19)))
 
 (let [data (->> "input10c"
                 slurp
                 (s/split-lines)
                 (map parse-instruction)
-                (reductions advance [1 [1 1]])
+                (reductions advance [1 [1]])
                 (map second)
-                (reduce concat))
+                (reduce concat)
+                (partition 40))
       part1 (->> data
-                 (map-indexed (fn [i v] (if (interesting? i) (* v i) nil)))
-                 (filter some?)
+                 (map-indexed interesting-value)
                  (reduce +))
       part2 (->> data
-                  rest
-                  (map-indexed (fn [i v] (if (covered? i v) "#" ".")))
-                  (partition 40)
-                  (mapv #(apply str %))
-                  (reduce #(str %1 "\n" %2)))]
+                 (map #(s/join "" (map-indexed draw-pixel %)))
+                 (s/join "\n"))
+      ]
   (printf "\npart1:%d, part2:\n%s" part1 part2))

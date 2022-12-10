f289bc2b53c95d926bf96e2b204e75c786262207
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-10 15:49:04 +0100

    na

--- src/bored/10.clj
+++ src/bored/10.clj
@@ -10,8 +10,7 @@
   (if op (let [ctr' (+ op ctr)] [ctr' [ctr ctr']])
       [ctr [ctr]]))
 
-(defn interesting? [i]
-  (= 20 (mod i 40)))
+(defn interesting? [i] (= 20 (mod i 40)))
 
 (defn covered? [i v] (#{1 0 -1} (- v (mod i 40))))
 
@@ -22,14 +21,14 @@
                 (reductions advance [1 [1 1]])
                 (map second)
                 (reduce concat))
-      first (->> data
+      part1 (->> data
                  (map-indexed (fn [i v] (if (interesting? i) (* v i) nil)))
                  (filter some?)
                  (reduce +))
-      second (->> data
+      part2 (->> data
                   rest
                   (map-indexed (fn [i v] (if (covered? i v) "#" ".")))
                   (partition 40)
                   (mapv #(apply str %))
                   (reduce #(str %1 "\n" %2)))]
-  (printf "\nfirst:%d, second:\n%s" first second))
+  (printf "\npart1:%d, part2:\n%s" part1 part2))

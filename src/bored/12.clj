4127c97d1ebc9f01aa433e995b2d3bb14d1c4bb0
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-13 13:52:05 +0100

    ..

--- src/bored/12.clj
+++ src/bored/12.clj
@@ -3,7 +3,9 @@
             [bored.heat :as h]))
 
 (defn V+ [[a A] [b B]] [(+ a b) (+ A B)])
+
 (defn V- [[a A] [b B]] [(+ a b) (+ A B)])
+
 (defn Vabs [[x y]] [(if (neg? x) (- x) x)
                     (if (neg? y) (- y) y)])
 (defn manhattan [a b]
@@ -18,10 +20,6 @@
 (def ^:dynamic terrain)
 (def ^:dynamic start)
 (def ^:dynamic end)
-
-;┌────┐
-;│ A* │
-;└────┘
 
 (defrecord trail [current val history comp])
 
@@ -41,27 +39,28 @@
         todo (sorted-set-by #(compare (:comp %1) (:comp %2)))]
     (loop [todos (conj todo starting)
            visited  (transient #{start})]
-      (let [choosen (first todos)]
-        (cond (= end (:current choosen)) choosen
-              (nil? choosen) nil
-              :else (recur (-> todos
-                               (disj choosen)
-                               (into (make-children visited choosen)))
-                           (conj! visited (:current choosen))))))))
+      (when-let [choosen (first todos)]
+        (if (= end (:current choosen)) choosen
+            (let [children (make-children visited choosen)]
+              (recur (-> todos
+                         (disj choosen)
+                         (into children))
+                     (reduce conj! visited (map :current children)))))))))
 
-(let [raw-data (->> "input12b"
-                    slurp
-                    (s/split-lines)
-                    (mapv #(mapv int %)))
-      [start-idx] (index-of2 raw-data (int \S))
-      [end-idx] (index-of2 raw-data (int \E))
-      data (-> raw-data
-               (assoc-in start-idx (int \a))
-               (assoc-in end-idx (int \z)))
-      calc  #(binding [terrain data
-                       start %
-                       end end-idx]
-               (count (:history (pathfind))))
-      candidates (for [i (range 41)] [i 0])]
-  {:first (calc start-idx)
-   :second (apply min (pmap calc candidates))})
+(time (let [raw-data (->> "input12b"
+                          slurp
+                          (s/split-lines)
+                          (mapv #(mapv int %)))
+            [start-idx] (index-of2 raw-data (int \S))
+            [end-idx] (index-of2 raw-data (int \E))
+            data (-> raw-data
+                     (assoc-in start-idx (int \a))
+                     (assoc-in end-idx (int \z)))
+            calc  #(binding [terrain data
+                             start %
+                             end end-idx]
+                     (count (:history (pathfind))))
+            candidates (index-of2 data (int \a)) ;; (for [i (range 41)] [i 0])
+            ]
+        {:first (calc start-idx)
+         :second (apply min (remove zero? (pmap calc candidates)))}))

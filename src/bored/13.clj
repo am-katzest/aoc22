b02b18c6c7c1d85ec2ad3a456f71b5f76375be83
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-13 13:58:56 +0100

    ..

--- src/bored/13.clj
+++ src/bored/13.clj
@@ -1,41 +1,37 @@
 (ns bored.13
   (:require [clojure.edn :as edn]))
 
-(defn ordered? [a b]
+(defn order [a b]
   (condp = [(int? a) (int? b)]
-    [true true] (compare b a)
+    [true true] (compare a b)
     [true false] (recur [a] b)
     [false true] (recur a [b])
     [false false]
     (let [[a1 & as] a
           [b1 & bs] b]
-      (condp = [(nil? a1) (nil? b1)]
-        [true true] 0
-        [true false] 1
-        [false true] -1
-        [false false]
-        (let [fst (ordered? a1 b1)]
-          (if (zero? fst) (recur as bs) fst))))))
+      (if (and a1 b1)
+        (let [fst (order a1 b1)]
+          (if (zero? fst) (recur as bs) fst))
+        (compare a b)))))
 
 (defn indices [f coll]
   (for [[i x] (map-indexed vector coll)
         :when (f x)]
     (inc i)))
 
-(def dividers #{[[2]] [[6]]})
-
 (let [data
       (->> "input13b"
            slurp
            (#(str "[" % "]"))
-           edn/read-string)]
+           edn/read-string)
+      dividers #{[[2]] [[6]]}]
   {:part1
    (->> data (partition 2)
-        (indices #(pos? (apply ordered? %)))
+        (indices #(neg? (apply order %)))
         (reduce +))
    :part2
    (->> data
-        (into dividers)
-        (sort (comp -  ordered?))
+        (concat dividers)
+        (sort order)
         (indices dividers)
         (reduce *))})

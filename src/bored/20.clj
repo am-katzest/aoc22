6cb662c6d19caf04ab6c07e172ca2d73234210eb
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-20 20:52:50 +0100

    ..

--- src/bored/20.clj
+++ src/bored/20.clj
@@ -8,44 +8,41 @@
     (if (= e s) [i (concat acc es)]
         (recur (inc i) es (conj acc e)))))
 
-(defn make-bi [xs]
-  [(into {} xs)
-   (into {} (map (fn [[a b]] [b a]) xs))])
+(defn insert-at [coll s idx]
+  (loop [i 0
+         [e & es] coll
+         acc []]
+    (if (= i idx) (concat acc (if e [s e] [s]) es)
+        (recur (inc i) es (conj acc e)))))
 
-(defn dissoc-key-bi [[k->v v->k] k]
-  [(dissoc k->v k)
-   (dissoc v->k (k->v k))])
-
-(defn assoc-key-bi [[k->v v->k] k v]
-  [(assoc k->v k v)
-   (assoc v->k v k)])
-
-(defn swap-keys-bi [[k->v v->k] a b]
-  (let [va (k->v a)
-        vb (k->v b)]
-    [(assoc k->v a vb b va)
-     (assoc v->k va b vb a)]))
-
-(defn roll [ms command]
-  (let [[i v] command
-        cnt (count (first ms))          ; O(1)
-        idx (get-in ms [1 command])
-        idx' (mod (+ idx v) cnt)]
-    (println v idx idx')
-    (swap-keys-bi ms idx idx')))
+(defn roll [coll command]
+  (let [[_ v] command
+        cnt (count coll)
+        [idx coll'] (remove-and-find-index coll command)
+        idx' (mod (+ idx v) (dec cnt))]
+    (insert-at coll' command idx')))
+(defn extract [coll]
+  (->> coll
+       (map second)
+       cycle
+       (drop-while #(not (zero? %)))
+       (partition 1000)
+       rest
+       (map first)
+       (take 3)
+       (reduce +)))
 
 (let [data (->> "input20a"
                 slurp
                 s/split-lines
-                (map #(Integer/parseInt %))
-                (map-indexed vector))
-      ms (->>  data (map-indexed vector) make-bi)]
-  (->> (reductions roll ms data)
-       (map #(->> %
-                  second
-                  (sort-by second)
-                  (map first)
-                  (map second)))
-
-       doall
-       (mapv println)))
+                (map #(Integer/parseInt %)))
+      data_1 (map-indexed (fn [i x] [i x]) data)
+      data_2 (map-indexed (fn [i x] [i (* x 811589153)]) data)]
+  {:part_1 (->> data_1
+                (reduce roll data_1)
+                extract)
+   :part_2 (->>
+            data_2
+            (iterate #(reduce roll % data_2))
+            (#(nth % 10))
+            extract)})

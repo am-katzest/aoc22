70edcc48ec59ae8e0124e4e320881fbdf0e76af8
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-21 23:36:36 +0100

    xd

--- src/bored/21.clj
+++ src/bored/21.clj
@@ -10,13 +10,9 @@
     [monkey (if b
               {:eq [b a c]}
               {:value (i a)})]))
-(read-monkey "rbzv:  gzpt - spng")
-
-(defn  replace-vals [coll f]
-  (into {} (map (fn [[k _]] [k (f)]) coll)))
 
 (defn monkeys [monkeys]
-  (let [mvars (replace-vals (into {} monkeys) lvar)]
+  (let [mvars (into {} (map (fn [[k _]] [k (lvar)]) monkeys))]
     (run 1 [q]
          (== q (mvars "root"))
          (and* (for [[name job] monkeys
@@ -32,22 +28,29 @@
                        "+" (fd/+ l r v)
                        "-" (fd/+ v r l)))))))))
 
+(defn monkeys2 [monkeys]
+  (let [mvars (into {} (map (fn [[k _]] [k (lvar)]) monkeys))]
+    (run 1 [q]
+         (== q (mvars "humn"))
+         (and* (for [[name job] monkeys
+                     :let [v (mvars name)]]
+                 (cond
+                   (= name "root")
+                   (let [[_ l r] (:eq job)]
+                     (== (mvars l) (mvars r)))
+                   (= name "humn") (== 0 0)
+                   (:value job) (== v (:value job))
+                   :else (let [[op l r] (:eq job)
+                               l (mvars l)
+                               r (mvars r)]
+                           (condp = op
+                             "*" (fd/* l r v)
+                             "/" (fd/* r v l)
+                             "+" (fd/+ l r v)
+                             "-" (fd/+ v r l)))))))))
+
 (->> "input21b"
      slurp
      s/split-lines
      (map read-monkey)
-     monkeys)
-
-(defn monkeys-dumb [monkeys]
-  (let [ms (into {} monkeys)
-        root (ffirst monkeys)
-        value (fn value [name]
-                (let [job (ms name)]
-                  (if-let [n (:value job)]
-                    n
-                    (let [[op l r] (:eq job)
-                          op ({"*" *' "+" +' "/" / "-" -'} op)
-                          l (value l)
-                          r (value r)]
-                      (op l r)))))]
-    (map value (map first monkeys))))
+     monkeys2)

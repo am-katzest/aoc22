9d7b9fc4d18cecc20f8f646ca11e037efb71fa3d
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-21 23:22:29 +0100

    ...

--- src/bored/21.clj
+++ src/bored/21.clj
@@ -10,16 +10,15 @@
     [monkey (if b
               {:eq [b a c]}
               {:value (i a)})]))
+(read-monkey "rbzv:  gzpt - spng")
 
 (defn  replace-vals [coll f]
   (into {} (map (fn [[k _]] [k (f)]) coll)))
 
 (defn monkeys [monkeys]
-  (let [mvars (replace-vals monkeys lvar)
-        vars (vals mvars)
-        root (mvars "root")]
+  (let [mvars (replace-vals (into {} monkeys) lvar)]
     (run 1 [q]
-         (== q vars)
+         (== q (mvars "root"))
          (and* (for [[name job] monkeys
                      :let [v (mvars name)]]
                  (if-let [n (:value job)]
@@ -29,14 +28,26 @@
                          r (mvars r)]
                      (condp = op
                        "*" (fd/* l r v)
-                       "/" (fd/* v r l)
+                       "/" (fd/* r v l)
                        "+" (fd/+ l r v)
-                       "-" (fd/+ l v r))))))
-         (everyg #(fd/in % (fd/interval Integer/MIN_VALUE Integer/MAX_VALUE)) vars))))
+                       "-" (fd/+ v r l)))))))))
 
-(->> "input21a"
+(->> "input21b"
      slurp
      s/split-lines
      (map read-monkey)
-     (into {})
      monkeys)
+
+(defn monkeys-dumb [monkeys]
+  (let [ms (into {} monkeys)
+        root (ffirst monkeys)
+        value (fn value [name]
+                (let [job (ms name)]
+                  (if-let [n (:value job)]
+                    n
+                    (let [[op l r] (:eq job)
+                          op ({"*" *' "+" +' "/" / "-" -'} op)
+                          l (value l)
+                          r (value r)]
+                      (op l r)))))]
+    (map value (map first monkeys))))

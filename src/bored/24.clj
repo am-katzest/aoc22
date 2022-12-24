6e9e2eabd76f4f1fca192d9bb07fff67bd36d2b0
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-24 15:08:03 +0100

    ez

--- src/bored/24.clj
+++ src/bored/24.clj
@@ -16,7 +16,7 @@
 (def ^:dynamic end)
 
 (defn read-terr [coll]
-  (into {:y (count coll) :x (count (first coll))}
+  (into {:y (dec (count coll)) :x (dec (count (first coll)))}
         (for [[y line] (map-indexed vector coll)
               [x cell] (map-indexed vector line)
               :let [c [x y]
@@ -28,9 +28,12 @@
           v)))
 (defn advance [coll]
   (let [dirs {\v [0 1] \< [-1 0] \> [1 0] \^ [0 -1]}
+        roll-1 (fn [max x] (cond (= 0 x) (dec max)
+                                 (= max x) 1
+                                 :else x))
         roll (fn [[x y]]
-               [(mod x (:x coll))
-                (mod y (:y coll))])
+               [(roll-1 (:x coll) x)
+                (roll-1 (:y coll) y)])
         moveable? (comp dirs second)
         m (filter moveable? coll)
         rest (into {} (remove moveable? coll))
@@ -46,32 +49,32 @@
 
 (defrecord trail [location time comp])
 
-(defn make-children [visited {:keys [location time]}]
+(defn make-children [{:keys [location time]}]
   (let [time' (inc time)
         blocked (terrain time')]
-    (for [dir [[0 1] [0 -1] [1 0] [-1 0]]
+    (for [dir [[0 1] [0 -1] [1 0] [-1 0] [0 0]]
           :let [loc' (V+ location dir)]
           :when (not (blocked loc'))
           :let [g time'
                 h (manhattan end loc')]]
       (->trail loc' time' [(+ g h) g loc' time']))))
 
-(defn pathfind []
-  (let [starting (->trail start 0 [])
-        todo (sorted-set-by #(compare (:comp %1) (:comp %2)))]
-    (loop [todos (conj todo starting)
-           visited  (transient #{[start 0]})]
-      (when-let [choosen (first todos)]
-        (println choosen)
-        (if (= end (:location choosen)) choosen
-            (let [children (make-children visited choosen)]
-              (recur (-> todos
-                         (disj choosen)
-                         (into children))
-                     (reduce conj! visited (map (fn [c] [(:location c) (:time c)]) children)))))))))
+(defn pathfind [s e start-time]
+  (binding [start s
+            end e]
+    (let [starting (->trail start start-time [])
+          todo (sorted-set-by #(compare (:comp %1) (:comp %2)))]
+      (loop [todos (conj todo starting)]
+        (when-let [choosen (first todos)]
+          (if (= end (:location choosen)) (:time choosen)
+              (let [children (make-children choosen)]
+                (recur (-> todos
+                           (disj choosen)
+                           (into children))))))))))
 
 (defonce advance' (memoize advance))
 (defonce grab-walls' (memoize grab-walls))
+
 (defn print-terr [coll]
   (println)
   (println)
@@ -82,19 +85,24 @@
       (println)
       (doseq [y (range (first ys) (inc (last ys)))]
         (print (if (coll [y x])  "{}" "  "))))))
-(let [init (->> "input24aa"
+
+(let [init (->> "input24a"
                 slurp
                 s/split-lines
                 read-terr)
       init (assoc init [(V+ (:start init) [0 -1]) \#] \#)
+      init (assoc init [(V+ (:end init) [0 1]) \#] \#)
       terr (->>
             init
             (iterate advance')
             (map grab-walls')
             (take 5000)
-            (into [])
-            time)]
-  (binding [terrain terr
-            start (:start init)
-            end (:end init)]
-    (print-terr (terrain 2))))
+            (into []))
+      s (:start init)
+      e (:end init)]
+  (time (binding [terrain terr]
+          (let [fst (pathfind s e 0)]
+            {:part1 fst
+             :part2 (->> fst
+                         (pathfind e s)
+                         (pathfind s e))}))))

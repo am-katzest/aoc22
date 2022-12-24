644a06cda09176ca570111d1cd4ed0639ae571bb
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-24 14:38:30 +0100

    ..

--- src/bored/24.clj
+++ src/bored/24.clj
@@ -11,7 +11,9 @@
 (defn manhattan [a b]
   (->> (V- a b) (Vabs) (apply +)))
 
-(def ^:dynamic terr)
+(def ^:dynamic terrain)
+(def ^:dynamic start)
+(def ^:dynamic end)
 
 (defn read-terr [coll]
   (into {:y (count coll) :x (count (first coll))}
@@ -42,14 +44,57 @@
                  (first k))))
        (into #{})))
 
+(defrecord trail [location time comp])
+
+(defn make-children [visited {:keys [location time]}]
+  (let [time' (inc time)
+        blocked (terrain time')]
+    (for [dir [[0 1] [0 -1] [1 0] [-1 0]]
+          :let [loc' (V+ location dir)]
+          :when (not (blocked loc'))
+          :let [g time'
+                h (manhattan end loc')]]
+      (->trail loc' time' [(+ g h) g loc' time']))))
+
+(defn pathfind []
+  (let [starting (->trail start 0 [])
+        todo (sorted-set-by #(compare (:comp %1) (:comp %2)))]
+    (loop [todos (conj todo starting)
+           visited  (transient #{[start 0]})]
+      (when-let [choosen (first todos)]
+        (println choosen)
+        (if (= end (:location choosen)) choosen
+            (let [children (make-children visited choosen)]
+              (recur (-> todos
+                         (disj choosen)
+                         (into children))
+                     (reduce conj! visited (map (fn [c] [(:location c) (:time c)]) children)))))))))
+
+(defonce advance' (memoize advance))
+(defonce grab-walls' (memoize grab-walls))
+(defn print-terr [coll]
+  (println)
+  (println)
+  (let [ys (sort (map first coll))
+        xs (sort (map second coll))]
+    (doseq [x (range (first xs)
+                     (inc (last xs)))]
+      (println)
+      (doseq [y (range (first ys) (inc (last ys)))]
+        (print (if (coll [y x])  "{}" "  "))))))
 (let [init (->> "input24aa"
                 slurp
                 s/split-lines
                 read-terr)
+      init (assoc init [(V+ (:start init) [0 -1]) \#] \#)
       terr (->>
             init
-            (iterate advance)
-            (map grab-walls)
-            (take 50)
-            (into []))]
-  terr)
+            (iterate advance')
+            (map grab-walls')
+            (take 5000)
+            (into [])
+            time)]
+  (binding [terrain terr
+            start (:start init)
+            end (:end init)]
+    (print-terr (terrain 2))))

a1cf773b609269d59b2d22a20d585fc048377848
bestestest kitteh <236551@edu.p.lodz.pl>
2022-12-26 19:32:38 +0100

    pretty

--- src/bored/16.clj
+++ src/bored/16.clj
@@ -1,16 +1,7 @@
 (ns bored.16
   (:require [clojure.string :as s]
-            [clojure.test :as t]
             [clojure.set :as set]))
 
-(defn unnil [num]
-  (let [a1 (int (/ num 26))
-        a2 (mod num 26)]
-    (apply str (map #(char (+ (int \A) %)) [a1 a2]))))
-(def nilcopy (map unnil (map #(nth % 2) '((4 . 541) (8 . 132) (11 . 129) (16 . 253) (19 . 264) (22 . 225) (25 . 397) (29 . 213)))))
-(def nilcopy2 (into {} (map-indexed (fn [i x] [x (- 100 i)]) nilcopy)))
-
-(defn replay [n] (get nilcopy2 n 0))
 (defn read-node [l]
   (let [[_ name _ _ _ x _ _ _ _ & destinations] (s/split l #"[ ,=;]+")]
     [name {:val (Integer/parseInt x)
@@ -47,7 +38,7 @@
         tresh (->> next-steps
                    (map first)
                    (apply max 0)
-                   (* 1/3))
+                   (* 1/2))
         next-steps (filter #(> (first %) tresh) next-steps)
         acc' (+ acc gain)]
     (if (seq next-steps)
@@ -69,26 +60,21 @@
        (map (fn [x] [x (set/difference s x)]))
        (filter (fn [[x y]] (pos? (compare (vec x) (vec y)))))))
 
-(time (let [raw-nodes (->> "input16b"
-                           slurp
-                           s/split-lines
-                           (map read-node))
-            starting "AA"
-            useless    (->> raw-nodes
-                            (remove (fn [[name {:keys [val]}]]
-                                      (or (< 0 val) (= name starting))))
-                            (map first))
-            nodes   (apply dissoc (complete (into {} raw-nodes)) useless)
-            avialable (disj (into #{} (keys nodes)) starting)]
-        {:part1 (best-path-heur starting nodes  avialable  30 0)
-         :part2 (->> avialable
-                     gen-sets
-                     (pmap  (fn [[x y]]
-                              (+ (best-path-heur starting nodes x  26 0)
-                                 (best-path-heur starting nodes y  26 0))))
-                     (apply max))}))
-
-;; a 1651
-;; b 2029
-;; c 1716
-;; d 2330
+(let [raw-nodes (->> "input16a"
+                     slurp
+                     s/split-lines
+                     (map read-node))
+      starting "AA"
+      useless    (->> raw-nodes
+                      (remove (fn [[name {:keys [val]}]]
+                                (or (< 0 val) (= name starting))))
+                      (map first))
+      nodes   (apply dissoc (complete (into {} raw-nodes)) useless)
+      avialable (disj (into #{} (keys nodes)) starting)]
+  {:part1 (best-path-heur starting nodes  avialable  30 0)
+   :part2 (->> avialable
+               gen-sets
+               (pmap  (fn [[x y]]
+                        (+ (best-path-heur starting nodes x  26 0)
+                           (best-path-heur starting nodes y  26 0))))
+               (apply max))})

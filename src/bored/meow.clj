b11ce25e5510fa1241542837014e9f30b29e4df6
bestestest kitteh <236551@edu.p.lodz.pl>
2023-02-20 23:15:01 +0100

    ..

--- src/bored/meow.clj
+++ src/bored/meow.clj
@@ -2,6 +2,15 @@
   (:require [clojure.string :as s]
             [clojure.test :as t]))
 
+(defn any-order [a b]
+  (str "(" a b "|" b a ")"))
+(defn at-least-one [main single]
+  (str "("
+       main "+" single main "*"
+       "|"
+       main "*" single main "+"
+       ")"))
+
 (def meows (str "(m[- ])?"
                 "("
                 (s/join "|"
@@ -9,10 +18,12 @@
                          "m+r+[ieao]+[łwu]+"
                          "m+[oae]?r*[łwu]*"
                          "p+u*rr+"
+                         "m[nmwreaouwi]+[włu]" ; this one should get most
+                         (str "r+" (at-least-one "[nmwreaouwi]" "[rea]") "[iawłu]+")
                          "m*r+((p|a+)?|r)" ; mrrrp mraa
                          ])
                 ")"))
-(def nyas "(n[- ])?n+[yi]+a+")
+(def nyas "(n[- ])?n+[mnyi]+a+")
 
 (def single-meowgex (str "(" meows "|" nyas ")"))
 (def multi-meowgex (str single-meowgex "([^a-zA-Z0-9]+" single-meowgex ")*"))
@@ -31,3 +42,13 @@ meowgex
   (doseq [meow must-meows]
     (t/is (= meow (-> single-meowgex re-pattern (re-matches meow) first)))))
 (filter #(some? (re-matches (re-pattern meowgex) %)) all-words)
+(remove #(some? (re-matches (re-pattern meowgex) %))
+        ["more"
+         "meorew"
+         "meoerwew"
+         "rmoewoermoww"
+         "rroreeoww"
+         "rmroeww"
+         "mreroew"
+         "mroew"])
+meowgex

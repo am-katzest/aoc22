b3812a2b5269cf91b4e771323134d4a524ee9571
bestestest kitteh <236551@edu.p.lodz.pl>
2023-03-19 17:23:04 +0100

    json

--- src/bored/uwu.clj
+++ src/bored/uwu.clj
@@ -1,6 +1,7 @@
 (ns bored.uwu
   (:require [clojure.xml :as xml]
-            [clojure.string :as  s]))
+            [clojure.string :as  s]
+            [clojure.data.json :as json]))
 
 (defn read-that-thing [x]
   (->> x
@@ -14,12 +15,44 @@
        xml/parse
        :content))
 
+(defn strip [x] (apply concat (map :content x)))
+(def unroll)
+(defn unex [t x]
+  (condp  = t
+    :DATE (first x)
+    :TITLE (first x)
+    :BODY (first x)
+    :DATELINE (first x)
+    :TEXT (unroll x)
+    (strip x)))
+
+(defn unroll [x]
+  (into {} (map (fn [{:keys [tag content]}] [tag (unex tag content)]) x)))
+
+(defn lower-keys [x]
+  (into {} (map (fn [[k v]] [(and k (-> k name s/lower-case keyword)) v]) x)))
+
+(defn parse [x]
+  (let [x (-> x
+              :content
+              unroll)]
+    (-> x
+        (dissoc :UNKNOWN :MKNOTE :TEXT)
+        (into (:TEXT x))
+        lower-keys)))
+
 (->> (range 0 22)
      (pmap read-that-thing)
      (reduce concat)
+     (pmap parse)
+     (remove #(% nil))
      shuffle
-     (partition-all 1000)
-     (pmap  #(with-out-str (mapv xml/emit-element %)))
-     (s/join "\n")
-     (spit "fixed")
+     json/write-str
+     (spit "json")
      time)
+(->> (range 0 1)
+     (pmap read-that-thing)
+     (reduce concat)
+     (pmap parse)
+     first
+     keys)
